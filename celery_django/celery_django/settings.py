473af55adf963cb326c92c93469b484231249e5c
Yuri <Jolycougar@gmail.com>
2024-10-02 21:57:40 +0300

    add celery to project

--- celery_django/celery_django/settings.py
+++ celery_django/celery_django/settings.py
@@ -119,6 +119,17 @@ EMAIL_HOST_USER = EMAIL_HOST_USER_test
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD_test
 EMAIL_PORT = 587
 
+REDIS_HOST = '0.0.0.0'
+REDIS_PORT = '6379'
+
+CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
