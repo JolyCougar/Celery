5e9b50aba3882524a9fa3dadf24b35c65c289ff0
Yuri <Jolycougar@gmail.com>
2024-10-03 21:11:20 +0300

    add celery task for spam in 5minute

--- celery_django/celery_django/celery.py
+++ celery_django/celery_django/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_django.settings')
@@ -7,3 +8,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_django.settings')
 app = Celery('celery_django')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'send-spam-every-5-minute': {
+        'task': 'send_mail.tasks.send_beat_email',
+        'schedule': crontab(minute='*/5'),
+    }
+}
--- celery_django/send_mail/tasks.py
+++ celery_django/send_mail/tasks.py
@@ -1,7 +1,21 @@
+from django.core.mail import send_mail
 from celery_django.celery import app
 from .service import send
+from .models import Contact
 
 
 @app.task
 def send_spam_email(user_email):
     send(user_email)
+
+
+@app.task
+def send_beat_email():
+    for contact in Contact.objects.all():
+        send_mail(
+            'Вы подписались на рассылку',
+            'Мы будем присылать вам много сообщений в течении 5 минут',
+            'danero95@yandex.ru',
+            [contact.email],
+            fail_silently=False,
+        )
